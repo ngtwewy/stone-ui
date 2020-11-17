1dc3b202b8ee578956e7382ab452007b31f53944
ngtwewy <62006464@qq.com>
2020-11-18 02:29:51 +0800

    feat 新增缩略图上传插件，图片列表上传插件

--- assets/javascripts/stoneui.js
+++ assets/javascripts/stoneui.js
@@ -93,111 +93,201 @@ this.stoneUI = {};
 
 
 
+/************************************************************************/
+/******************************** 分割线 *********************************/
+/************************************************************************/
 
-/*!
-* Alert.js
-*
+/** 缩略图插件
 * @author   ngtwewy <https://www.restfulapi.cn>
 * @license  Apache 2
-* @time     2018-09-21
+* @time     2018-10-14
 */
 
-/* 生成如下Html，然后自动删除
-<div class="alert alert-danger my-alert" role="alert">
-  <a href="#" class="alert-link">
-    依赖警告框 JavaScript 插件
-  </a>
-</div>
-*/
-
-(function () {
-  var options = {
-    message:'默认 message',
-    type: 'alert-success',
-    seconds: 3,
-    url: null
-	}
-  
-  //helper
-	var getType = function(arg){
-    var type;
-	  switch(arg){
-      case "success": type = "alert-success"; 
-        break;
-      case "info":    type = "alert-info"; 
-        break;
-      case "warning": type = "alert-warning"; 
-        break;
-      case "danger":  type = "alert-danger"; 
-        break;
-      default:        type = "alert-success"; 
-    }
-    return type;
-  }
-  
-  var alertShow = function (msg, type, url, seconds) {
-    //创建警告框
-    var fragment = document.createDocumentFragment();
-    var div = document.createElement("div");
-    div.classList.add("alert");
-    div.classList.add(getType(type));
-    div.classList.add("my-alert");
-
-    var text = document.createTextNode(msg);
-    div.appendChild(text);
-    fragment.appendChild(div);
-    document.querySelector("body").appendChild(fragment);
-
-    seconds = seconds ? seconds * 1000 : 2 * 1000;
-    //隐藏警告框
-    setTimeout(function () {
-      div.classList.add("my-alert");
-      div.classList.add("my-alert-hide");
-    }, seconds);
-
-    //删除警告框
-    setTimeout(function () {
-      document.querySelector("body").removeChild(div);
-      // document.querySelectorAll(".my-alert").forEach(function (item) {
-      //   document.querySelector("body").removeChild(item);
-      // });
-    }, seconds + 300);
-
-    //跳转
-    setTimeout(function () {
-      if (url) {
-        location.href = url;
+(function(){
+	//所有缩略图绑定
+	var thumbnailContainers = document.querySelectorAll(".thumbnail-panel");
+  if (thumbnailContainers) {
+		console.log("ssss: ", thumbnailContainers);
+    thumbnailContainers.forEach(function (item) {
+			console.log("item ", item);
+      item.querySelector('.upload-button').addEventListener('click', thumbnailAction);
+      item.querySelector('.delete-button').addEventListener('click', thumbnailDelete);
+      //检测是否有缩略图，有的话, 显示缩略图，同时显示删除按钮
+      if (item.querySelector('.input-hidden').value) {
+				var imgUrl = item.querySelector('.project-thumbnail img').getAttribute("data-thumbnail") 
+											+ item.querySelector('.input-hidden').value;
+        item.querySelector('.project-thumbnail img').src = imgUrl;
+        item.querySelector('.delete-button').style.display = 'block';
       }
-    }, seconds + 300);
+    });
   }
+  //上传按钮事件
+  function thumbnailAction() {
+    let fileInput = this.parentNode.querySelector("input[name='file'].upload-input");
+    let url = fileInput.getAttribute("data-url");
 
-  //API
-	var api = {
-		config: function (opts) {
-			if(!opts) return options;
-			for(var key in opts) {
-				options[key] = opts[key];
-      }
-			return this;
-		},
+    fileInput.click();
+    fileInput.onchange = function () {
+      if (!this.files[0] || this.files[0] == undefined) return;
 
-		listen: function listen(elem) {
-			if (typeof elem === 'string') {
-				var elems = document.querySelectorAll(elem),
-					      i = elems.length;
-        while (i--) {
-          listen(elems[i]);
+      var fd = new FormData();
+      fd.append("file", this.files[0]);
+      // fd.append("project", 123);
+
+      axios({
+        method: 'post',
+        url: url,
+        data: fd,
+        headers: { 'content-type': 'multipart/form-data' },
+      }).then(function (response) {
+        if (response.data.error == 0) {
+          fileInput.parentNode.parentNode.querySelector('.input-hidden').value = response.data.data.thumbnail;
+          fileInput.parentNode.parentNode.querySelector('.project-thumbnail img').src = response.data.data.url;
+          fileInput.parentNode.parentNode.querySelector(".delete-button").style.display = 'block';
+        }else{
+          myAlert.show(response.data.msg, 'danger');
+          fileInput.value="";
         }
-        return
-			}
-			return this;
-		},
-
-    show: function(msg, type, url, seconds){
-      alertShow(msg, type, url, seconds);
+      });
     }
-	}
+  }
 
-	this.stoneUI.alert = api;
+  //删除按钮事件
+  function thumbnailDelete() {
+    var thumbnail = this.parentNode.querySelector(".project-thumbnail img");
+    thumbnail.src = thumbnail.getAttribute("data-nothumbnail");
+    this.parentNode.parentNode.querySelector('.input-hidden').value = "";
+    this.style.display = 'none';
+  }
 })();
+
+
+
+/************************************************************************/
+/******************************** 分割线 *********************************/
+/************************************************************************/
+/**
+ * 上传图片列表
+ */
+
+ /**
+  <div class="image-item">
+      <input type="text" name="image_name[]" value="{$vo.name}" class="form-control">
+      <input type="hidden" name="image_url[]" value="{$vo.url}" class="form-control">
+      <img src="{$vo.url}">
+      <a href="javascript:;" class="btn btn-warning image-btn-update">替换</a>
+      <a href="javascript:;" class="btn btn-danger image-btn-delete">删除</a>
+  </div>
+
+  <a class="btn btn-primary btn-sm image-btn-add" href="javascript:;">添加图片</a>
+  <input type="file" name="image_input" style="display:none;" data-url='{:url("asset/Image/uploadThumbnail")}'>
+  */
+
+
+ (function(){
+  //上传按钮事件
+  var imagesAction = function () {
+    var that = this;
+    let fileInput = this.parentNode.querySelector("input[name='image_input']");
+    let url = fileInput.getAttribute("data-url");
+
+    fileInput.click();
+    fileInput.onchange = function () {
+      if (!this.files[0] || this.files[0] == undefined) return;
+
+      var fd = new FormData();
+      fd.append("file", this.files[0]);
+
+      axios({
+        method: 'post',
+        // url: url + '?resize=true',
+        url: url,
+        data: fd,
+        headers: { 'content-type': 'multipart/form-data' },
+      }).then(function (response) {
+        if (response.data.error == 0) {
+          var data = response.data.data;
+          console.log(data);
+          var image_item = '\
+                <div class="image-item">\
+                    <input type="text" name="image_name[]" var="" class="form-control">\
+                    <input type="hidden" name="image_url[]" value="'+ data.thumbnail + '" class="form-control">\
+                    <img src="'+ data.url + '">\
+                    <a href="javascript:;" class="btn btn-warning image-btn-update">替换</a>\
+                    <a href="javascript:;" class="btn btn-danger image-btn-delete">删除</a>\
+                </div>\
+                ';
+
+          var ss = document.createElement("div");
+          ss.innerHTML = image_item;
+          var add_btn = that.parentNode.querySelector(".image-btn-add");
+          add_btn.parentNode.insertBefore(ss, add_btn);
+        } else {
+          console.log("图片上传错误");
+        }
+      });
+    }
+  }
+  var image_btn_add = document.querySelectorAll(".image-btn-add");
+  if(image_btn_add){
+    image_btn_add.forEach(function(btn){
+      btn.addEventListener('click', imagesAction);
+    });
+  }
+  
+
+  // 删除
+  var image_btn_delete = document.querySelectorAll(".image-btn-delete");
+  image_btn_delete.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      console.log("del");
+      this.parentNode.parentNode.removeChild(this.parentNode);
+    });
+  });
+
+  // 修改
+  //上传按钮事件
+  var updateAction = function () {
+    let fileInput = this.parentNode.parentNode.querySelector("input[name='image_input']");
+    let url = fileInput.getAttribute("data-url");
+
+    let image_name = this.parentNode.querySelector('input[type="text"]');
+    let image_url = this.parentNode.querySelector('input[type="hidden"]');
+    let img = this.parentNode.querySelector('img');
+
+    fileInput.click();
+    fileInput.onchange = function () {
+      if (!this.files[0] || this.files[0] == undefined) return;
+
+      var fd = new FormData();
+      fd.append("file", this.files[0]);
+
+      axios({
+        method: 'post',
+        url: url,
+        data: fd,
+        headers: { 'content-type': 'multipart/form-data' },
+      }).then(function (response) {
+        if (response.data.error == 0) {
+          var data = response.data.data;
+          console.log(data);
+          img.src = data.url;
+          image_url.value = data.thumbnail;
+        } else {
+          console.log("图片上传错误");
+        }
+      });
+    }
+  }
+  // 删除
+  var image_btn_update = document.querySelectorAll(".image-btn-update");
+  if(image_btn_update){
+    image_btn_update.forEach(function (btn) {
+      btn.addEventListener('click', updateAction);
+    });
+  }
+})();
+
+
+
 
